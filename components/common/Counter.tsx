9bbcdaccece33fade7c10ea18de23d28bb558a6c
AkashKumarRam <heyakashkumarram@gmail.com>
2024-04-13 16:18:47 +0530

    Update Animated Number Count Code

--- components/common/Counter.tsx
+++ components/common/Counter.tsx
@@ -2,31 +2,28 @@
 import React, { useEffect, useState } from "react";
 import AnimatedNumbers from "react-animated-numbers";
 
-
 export default function Counter({ num }: { num: number }) {
-    const [show, setShow] = useState<boolean>(false);
+  const [show, setShow] = useState<boolean>(false);
 
-    useEffect(() => {
-        setShow(true);
-    }, []);
-    return (
-        <>
-            {show && (
-                <AnimatedNumbers
-                    includeComma
-                    animateToNumber={num}
-                    fontStyle={{ fontSize: 40, fontWeight: "bold" }}
-                    locale="en-US"
-                    configs={[
-                        { mass: 1, tension: 220, friction: 100 },
-                        { mass: 1, tension: 180, friction: 130 },
-                        { mass: 1, tension: 280, friction: 90 },
-                        { mass: 1, tension: 180, friction: 135 },
-                        { mass: 1, tension: 260, friction: 100 },
-                        { mass: 1, tension: 210, friction: 180 },
-                    ]}
-                ></AnimatedNumbers>
-            )}
-        </>
-    );
+  useEffect(() => {
+    setShow(true);
+  }, []);
+  return (
+    <>
+      {show && (
+        <AnimatedNumbers
+        includeComma
+        transitions={(index) => ({
+          type: "spring",
+          duration: index + 0.3,
+        })}
+        animateToNumber={num}
+        fontStyle={{
+          fontSize: 40,
+          color: "red",
+        }}
+      />
+      )}
+    </>
+  );
 }
