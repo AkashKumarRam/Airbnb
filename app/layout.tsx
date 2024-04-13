f91328e1461f607aea6cbad3cdf4f381b9a67664
AkashKumarRam <heyakashkumarram@gmail.com>
2024-04-13 15:09:59 +0530

    Include Deploy Command

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
   description: "Airbnb | Holiday rentals, cabins, beach houses & more",
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: Readonly<{
